d9117b778050598940dc3ddfc80c2152a173cdc4
Zachary Lee <w.zachary.lee@gmail.com>
2024-08-15 13:51:12 -0400

    analytics

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
@@ -56,6 +57,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           <ThemeProvider attribute="class" defaultTheme="light">
             <TRPCReactProvider>{props.children}</TRPCReactProvider>
             <Toaster />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
